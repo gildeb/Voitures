44e8cd85eda02e9383d4bc85903073a8863d1c98
gildeb <48280441+gildeb@users.noreply.github.com>
2024-04-01 18:53:35 +0200

    Add files via upload

--- Robot Service Jeunesse 2024/ESP32-cam/Micropython/RSJ2024_image_server.py
+++ Robot Service Jeunesse 2024/ESP32-cam/Micropython/RSJ2024_image_server.py
@@ -3,17 +3,16 @@ from machine import Pin, reset
 import camera
 from neopixel import NeoPixel
 from time import sleep_ms
-
+#
+robot_no = 1
+#
 color_dict = {  b'GREEN':(255, 0, 0)
          , b'RED'  :(0, 255, 0)
          , b'BLUE' :(0, 0, 255)
          , b'BLACK':(0, 0, 0)
            }
-# count = 0
 # camera initialization
 camera.init(0, format=camera.JPEG, fb_location=camera.PSRAM)
-# camera.framesize(camera.FRAME_QVGA)    # 320x240
-# camera.framesize(camera.FRAME_QQVGA)   # 160x120
 camera.framesize(camera.FRAME_96X96)
 camera.quality(10)
 #
@@ -21,18 +20,16 @@ np = NeoPixel(Pin(12), 1)
 np[0] = color_dict[b'BLACK']
 np.write()
 #
-onboard_led = Pin(4)
-#
-led = Pin(4, Pin.OUT)
+onboard_led = Pin(4, Pin.OUT)
 for _ in range(2):
-    led.on()
+    onboard_led.on()
     sleep_ms(300)
-    led.off()
+    onboard_led.off()
     sleep_ms(300)
 # activate access point
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
-ap.config(essid="ESP32-cam-1", password="")
+ap.config(essid="ESP32-cam-{}".format(robot_no), password="")
 # activate server
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
@@ -46,8 +43,6 @@ while True:
         np.write()
         buf = camera.capture()
         s.sendto(buf, cmd[1])
-#        count += 1
-#        print('image ', count)
     except KeyboardInterrupt:
         break
 #
