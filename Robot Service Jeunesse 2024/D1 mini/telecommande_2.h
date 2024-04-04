c41546bf9efc45e3c02b864d3326edaf10b21a95
gildeb <48280441+gildeb@users.noreply.github.com>
2024-04-04 12:05:08 +0200

    Add files via upload

--- Robot Service Jeunesse 2024/D1 mini/telecommande_2.h
+++ Robot Service Jeunesse 2024/D1 mini/telecommande_2.h
@@ -1,15 +1,12 @@
-//const int ARRET  = 0x1FE817E;  //arrêt des moteurs
-//const int ACCELG = 0x1FEE11E;  //bouton VOL+ : accélération moteur gauche
-//const int FREING = 0x1FE916E;  //bouton VOL- : décélération moteur gauche
-const int AVT1   = 0xFF18E7;  //bouton HAUT : marche avant
-const int ARR1   = 0xFF4AB5;  //bouton BAS  : marche arrière
-const int VIREG  = 0xFF10EF;  //bouton GAUCHE : ralentissement moteur gauche pendant 1s
-const int VIRED  = 0xFF5AA5;  //bouton DROITE : ralentissement moteur droit pendant 1s
-//const int PAGE   = 0x1FEC13E;  //bouton PAGE : marche avant à Vmini pendant 1s puis arret des //moteurs
-//const int HOME   = 0x1FECE31;  //bouton HOME : marche avant à Vmini pendant 0.2s puis arret des moteurs
-//const int OKOK   = 0x1FEC837;  //bouton OK : arret des moteurs
-//const int RETOUR = 0x1FEE41B;  //bouton RETOUR : remettre DCG et DCD à DC
-//const int SOURIS = 0x1FE12ED;  //bouton SOURIS : allumage/extinction led ESP32CAM
-
-//#define STOP1  0xFF38C7
-//#define ARRET1  0xFFA25D
+const int ARRET  = 0x807F02FD;  //arrêt des moteurs
+const int ACCELG = 0x807F18E7;  //bouton VOL+ : accélération moteur gauche
+const int FREING = 0x807F08F7;  //bouton VOL- : décélération moteur gauche
+const int AVT1   = 0x807F6897;  //bouton HAUT : marche avant
+const int ARR1   = 0x807F58A7;  //bouton BAS  : marche arrière
+const int VIREG  = 0x807F8A75;  //bouton GAUCHE : ralentissement moteur gauche pendant 1s
+const int VIRED  = 0x807F0AF5;  //bouton DROITE : ralentissement moteur droit pendant 1s
+const int PAGE   = 0x807F32CD;  //bouton PAGE : marche avant à Vmini pendant 1s puis arret des moteurs
+const int HOME   = 0x807F8877;  //bouton HOME : marche avant à Vmini pendant 0.2s puis arret des moteurs
+const int OKOK   = 0x807FC837;  //bouton OK : arret des moteurs
+const int RETOUR = 0x807F9867;  //bouton RETOUR : remettre DCG et DCD à DC
+const int SOURIS = 0x807F00FF;  //bouton SOURIS : allumage/extinction led ESP32CAM
