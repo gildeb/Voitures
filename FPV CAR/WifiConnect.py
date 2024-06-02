098900cc37a84b797eb3688bd8ea9e44cdcfe920
gildeb <48280441+gildeb@users.noreply.github.com>
2024-06-02 11:53:51 +0200

    Update WifiConnect.py

--- FPV CAR/WifiConnect.py
+++ FPV CAR/WifiConnect.py
@@ -14,12 +14,7 @@ from time import sleep
 import sys
 
 def WifiConnect(lan=None):
-    credentials = { 'KIWI_11091':'iSpEnJ2q',
-                    'iPhone de Gilles':'sx8tuu5zx1jdr',
-                    'Livebox-FDB5':'33311297193A5A1733FF813783',
-                    'Wifi-Eleves':'a$@0910626L',
-#                     'Wifi-Profs':'&*%@0910626L',
-                    'Bbox-0989EBA7':'Zppcz1UU61wwp7P2VN'}
+    credentials = { 'ssid':'mypwd'}
 
     ap_if = network.WLAN(network.AP_IF)
     ap_if.active(False)                     # disable wifi access point
